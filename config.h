1e2e1632054b8a95e710ea567c21f78831382608
hashim <adminofv@proton.me>
2025-01-19 16:36:39 -0500

    Added bigger gaps

--- config.h
+++ config.h
@@ -41,7 +41,7 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
-
+static const unsigned int gappx     = 14; /* Adding window gaps */
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
